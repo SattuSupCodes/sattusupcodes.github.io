30c39cf6b9fcad29a878c51a2a4d3d35cfa3b3d1
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-04 22:25:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  base:"https://sattusupcodes.github.io/3D_Portfolio/"
   
 })
